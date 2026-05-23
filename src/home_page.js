4244d33ea2a68aef666da1e9af5ef832a6da97b1
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-05-23 18:14:54 +0800

    feat: render home page content on home button click

--- src/home_page.js
+++ src/home_page.js
@@ -1,21 +1,30 @@
 import phoImage from './pho.jpg'
 
 function displayHome(contentBox) {
-    const headline = document.createElement('h1');
-    headline.textContent = 'Vietnamese fuds 🇻🇳';
+    const homePageDiv = document.createElement('div');
+    homePageDiv.id = 'home-page'
+
+    const restaurantName = document.createElement('h1');
+    restaurantName.textContent = '🇻🇳Welcome to Pho restaurant🍜';
 
     const phoImg = document.createElement('img')
     phoImg.src = phoImage;
+    phoImg.style.width = '600px';
+    phoImg.style.height = '400px';
 
     const phoDesc = document.createElement('p')
-    phoDesc.textContent = 'This is one of the most popular and delicious dish in Vietnam'
+    phoDesc.textContent = 'One of the most popular and delicious dish in Vietnam'
+
+    // add to the home page div
+    homePageDiv.appendChild(restaurantName);
+    homePageDiv.appendChild(phoImg);
+    homePageDiv.appendChild(phoDesc);
 
     // add to the content box
-    contentBox.appendChild(headline);
-    contentBox.appendChild(phoImg);
-    contentBox.appendChild(phoDesc);
+    contentBox.appendChild(homePageDiv);
 }
 
+// named export
 export { displayHome };
 
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import './styles.css'
 import { displayHome } from './home_page.js';
 
 const contentBox = document.querySelector('#content');
-displayHome(contentBox);
+const homeBtn = document.querySelector('#home-btn');
+
+homeBtn.addEventListener('click', () => {
+    displayHome(contentBox);
+});
